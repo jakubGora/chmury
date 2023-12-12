1e3d28995e5c820a6daa471ae4bff92566417cbc
Jakub Góra <jakub.gora@sts.pl>
2023-12-12 15:23:43 +0100

    init gcloud

--- src/app/services/tutorial.service.ts
+++ src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'https://backend-dot-cellular-codex-407313.lm.r.appspot.com/api/turorials';
+const baseUrl = 'https://backend-dot-cellular-codex-407313.lm.r.appspot.com/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
